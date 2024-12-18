38d588bb9d8320ed3585f11e007ca82b0e39d1a1
Naseeb Naushad <150791316+naseebnaushad@users.noreply.github.com>
2024-12-18 16:16:33 +0000

    Update mdm.sh

--- mdm.sh
+++ mdm.sh
@@ -4,18 +4,18 @@ GRN='\033[0;32m'
 BLU='\033[0;34m'
 NC='\033[0m'
 echo ""
-echo -e "Auto Tools for MacOS"
+echo -e "Auto Tools for MacOS - Targeting maverick Volume"
 echo ""
 PS3='Please enter your choice: '
-options=("Bypass on Recovery" "Disable Notification (SIP)" "Disable Notification (Recovery)" "Check MDM Enrollment" "Exit")
+options=("Bypass on Recovery (maverick)" "Disable Notification (SIP) (maverick)" "Disable Notification (Recovery) (maverick)" "Check MDM Enrollment (maverick)" "Exit")
 select opt in "${options[@]}"; do
 	case $opt in
-	"Bypass on Recovery")
-		echo -e "${GRN}Bypass on Recovery"
+	"Bypass on Recovery (maverick)")
+		echo -e "${GRN}Bypass on Recovery for maverick"
 		if [ -d "/Volumes/maverick - Data" ]; then
    			diskutil rename "maverick - Data" "Data"
 		fi
-		echo -e "${GRN}Create a new user"
+		echo -e "${GRN}Create a new user on maverick"
         echo -e "${BLU}Press Enter to move to the next step, if not filled in, it will automatically receive the default value"
   		echo -e "Enter username (Default: MAC)"
 		read realName
@@ -26,53 +26,51 @@ select opt in "${options[@]}"; do
   		echo -e "${BLUE}Enter password (default: 1234)"
     	read passw
       	passw="${passw:=1234}"
-		dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default' 
-        echo -e "${GREEN}Creating users"
+		dscl_path='/Volumes/maverick/private/var/db/dslocal/nodes/Default' 
+        echo -e "${GREEN}Creating users on maverick"
   		# Create user
     	dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
       	dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShell "/bin/zsh"
 	    dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
-	 	dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
-	    dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
+	 	dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
 	    dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID "20"
-		mkdir "/Volumes/Data/Users/$username"
+		mkdir "/Volumes/maverick/Users/$username"
 	    dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
 	    dscl -f "$dscl_path" localhost -passwd "/Local/Default/Users/$username" "$passw"
 	    dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
-		echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/maverick/etc/hosts
-		echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/maverick/etc/hosts
-		echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/maverick/etc/hosts
-        echo -e "${GREEN}Successfully block hosts${NC}"
-		# echo "Remove config profile"
-  	touch /Volumes/Data/private/var/db/.AppleSetupDone
+		echo "0.0.0.0 deviceenrollment.apple.com" >> /Volumes/maverick/etc/hosts
+		echo "0.0.0.0 mdmenrollment.apple.com" >> /Volumes/maverick/etc/hosts
+		echo "0.0.0.0 iprofiles.apple.com" >> /Volumes/maverick/etc/hosts
+        echo -e "${GREEN}Successfully blocked MDM enrollment hosts on maverick${NC}"
+		# Remove config profile
+  		touch /Volumes/maverick/private/var/db/.AppleSetupDone
         rm -rf /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-	rm -rf /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-	touch /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-	touch /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+		rm -rf /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+		touch /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+		touch /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
 		echo "----------------------"
 		break
 		;;
-    "Disable Notification (SIP)")
-    	echo -e "${RED}Please Insert Your Password To Proceed${NC}"
-        sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-        sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-        sudo touch /var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-        sudo touch /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+    "Disable Notification (SIP) (maverick)")
+    	echo -e "${RED}Please Insert Your Password To Proceed on maverick${NC}"
+        sudo rm /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+        sudo rm /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+        sudo touch /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+        sudo touch /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
         break
         ;;
-    "Disable Notification (Recovery)")
+    "Disable Notification (Recovery) (maverick)")
         rm -rf /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-	rm -rf /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-	touch /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-	touch /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
-
+		rm -rf /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+		touch /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+		touch /Volumes/maverick/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
         break
         ;;
-	"Check MDM Enrollment")
+	"Check MDM Enrollment (maverick)")
 		echo ""
-		echo -e "${GRN}Check MDM Enrollment. Error is success${NC}"
+		echo -e "${GRN}Check MDM Enrollment for maverick${NC}"
 		echo ""
-		echo -e "${RED}Please Insert Your Password To Proceed${NC}"
+		echo -e "${RED}Please Insert Your Password To Proceed on maverick${NC}"
 		echo ""
 		sudo profiles show -type enrollment
 		break
